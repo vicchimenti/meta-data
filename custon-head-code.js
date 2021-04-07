5a302b07b3ac1f4db7e5f8253b9da04601262661
vicchimenti <chimenti@seattleu.edu>
2021-04-07 10:43:37 -0700

    add custom head code

--- custon-head-code.js
+++ custon-head-code.js
@@ -1,33 +1,35 @@
 /***
-*     @author Victor Chimenti, MSCS-SE '20
-*     @file custom-head-code.js
-*
-*     This document will format the tags necessary to pull metadata from the
-*     source and insert it into the head of a page layout.
-*
-*     The doc will need to be customized to any content type that uses 
-*     the custom head code layout. It is to be used in conjunction with a
-*     nav object which is coded into the page head.
-*
-*     Document will write once when the page loads
-*
-*     @version 1.0
-*/
+ *     @author Victor Chimenti, MSCS-SE '20
+ *     @file custom-head-code.js
+ *
+ *     This document will format the tags necessary to pull metadata from the
+ *     source and insert it into the head of a page layout.
+ *
+ *     The doc will need to be customized to any content type that uses 
+ *     the custom head code layout. It is to be used in conjunction with a
+ *     nav object which is coded into the page head.
+ *
+ *     Document will write once when the page loads
+ *
+ *     @version 1.1
+ */
 
 
 
 
 try {
-    
-    var metaType = com.terminalfour.publish.utils.BrokerUtils.processT4Tags(dbStatement, publishCache, section, content, language, isPreview, "<meta name='og:type' property='article' />");
-    var metaTitleOG = com.terminalfour.publish.utils.BrokerUtils.processT4Tags(dbStatement, publishCache, section, content, language, isPreview, "<meta name='og:title' content='<t4 type=\'content\' name=\'Article Title\' output=\'normal\' modifiers=\'striptags,htmlentities\' />' />");
-    var metaDescriptionOG = com.terminalfour.publish.utils.BrokerUtils.processT4Tags(dbStatement, publishCache, section, content, language, isPreview, "<meta name='og:description' content='<t4 type=\'content\' name=\'Summary\' output=\'normal\' modifiers=\'striptags,htmlentities\' />' />");
+
+    var headTitle = com.terminalfour.publish.utils.BrokerUtils.processT4Tags(dbStatement, publishCache, section, content, language, isPreview, "<t4 type='content' name='Meta Title' output='normal' modifiers='striptags,htmlentities' />");
+    var headTitleString = "<title><t4 type='title' /></title>";
 
 
 
-    document.write(com.terminalfour.publish.utils.BrokerUtils.processT4Tags(dbStatement, publishCache, section, content, language, isPreview, metaType));
-    document.write(com.terminalfour.publish.utils.BrokerUtils.processT4Tags(dbStatement, publishCache, section, content, language, isPreview, metaTitleOG));
-    document.write(com.terminalfour.publish.utils.BrokerUtils.processT4Tags(dbStatement, publishCache, section, content, language, isPreview, metaDescriptionOG));
+    if (headTitle != "") {
+        headTitleString = "<title>" + headTitle + "</title>";
+    }
+
+    document.write(com.terminalfour.publish.utils.BrokerUtils.processT4Tags(dbStatement, publishCache, section, content, language, isPreview, headTitleString));
+
 
 
 } catch (err) {

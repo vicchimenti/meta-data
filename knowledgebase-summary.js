69fcf80908d2abb004d6817798750949ec3f34bf
vicchimenti <victor.chimenti@gmail.com>
2022-09-23 09:36:54 -0700

    pull cm code from kb summary

---
+++ knowledgebase-summary.js
@@ -0,0 +1,23 @@
+/*
+<div class="knowledgeBaseItemWrapper" id="id<t4 type='meta' meta='content_id' />">
+  <div class="knowledgeBaseItem standardContent">
+    <h4>
+      <a href="<t4 type='content' name='Article' output='fulltext' use-element='true' filename-element='' modifiers='medialibrary,nav_sections' />"><t4 type="content" name="Name" output="normal" modifiers="striptags,htmlentities" /></a>
+    </h4>
+
+    <t4 type="content" name="Summary" output="selective-output" modifiers="medialibrary,nav_sections" process-format="true" format="  <div class=&quot;objective&quot;>$value</div>" />
+    
+    <ul class="tags">
+      <t4 type="content" name="Tags3" output="selective-output" format="<li>$value</li>" /><br>
+      <t4 type="content" name="Tags3" output="normal" display_field="value" /><br>
+      <li>Email</li>
+      <li>MacOS</li>
+    </ul>
+    
+    <div class="lastModified" style="display:inline-block">Last modified: <t4 type="meta" meta="last_modified" format="MMMM d, yyyy" />
+    </div>
+  
+    <div class="keywords" style="display:none;" aria-hidden="true"><t4 type="content" name="Searchable Keyword" output="normal" modifiers="striptags" /></div>
+  </div>
+</div>
+*/
